7b353a0e42a8711c48511ef46bbc0ff450c0e2fb
Cristian Gabbanini <cristian.gabbanini@gmail.com>
2019-06-03 11:06:37 +0200

    Add lesson 1 solution

--- src/api/project/project.resolvers.js
+++ src/api/project/project.resolvers.js
@@ -1,6 +1,19 @@
 // resolve the query you made
 module.exports = {
   Query: {
-    
+    welcome() {
+      return {
+        message: "Hello world!"
+      };
+    }
+  },
+  Mutation: {
+    createAnimal(_, args) {
+      const { name, species } = args.input;
+      return {
+        name,
+        species
+      };
+    }
   }
-}
+};
